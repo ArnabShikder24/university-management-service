2cb7848c144a5b3f4aa958dc1c0487c939cd49a9
ArnabShikder24 <arnabshikder2001@gmail.com>
2023-06-03 21:14:45 +0600

    project fully setup

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+const PORT = config.PORT;
+
+async function mainServer() {
+    try {
+        await mongoose.connect(config.database_url as string);
+        console.log(`Database connection successfully`);
+
+        app.listen(PORT, () => console.log('Server Running on ', PORT));
+    } catch (error) {
+        console.log('Database connection failed!!!', error);
+    }
+  
+};
+
+mainServer();
