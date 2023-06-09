28fd173132bc26d5a6ff319ef5cd999bf1e121fb
ArnabShikder24 <arnabshikder2001@gmail.com>
2023-06-09 18:03:02 +0600

    prettier setup properly

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,17 @@
-import express, { Application, Request, Response } from 'express';
-import cors from 'cors';
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 
-const app: Application = express();
+const app: Application = express()
 
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req: Request, res: Response) => {
-    res.send({
-        status: true,
-        message: 'server running...'
-    })
+  res.send({
+    status: true,
+    message: 'server running...',
+  })
 })
-
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,18 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
-const PORT = config.PORT;
+const PORT = config.PORT
 
 async function mainServer() {
-    try {
-        await mongoose.connect(config.database_url as string);
-        console.log(`Database connection successfully`);
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log(`Database connection successfully`)
 
-        app.listen(PORT, () => console.log('Server Running on ', PORT));
-    } catch (error) {
-        console.log('Database connection failed!!!', error);
-    }
-  
-};
+    app.listen(PORT, () => console.log('Server Running on ', PORT))
+  } catch (error) {
+    console.log('Database connection failed!!!', error)
+  }
+}
 
 mainServer();
