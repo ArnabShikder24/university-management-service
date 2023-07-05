ccf5a18bb066a054c3388e4a9dcabb440e065dcb
ArnabShikder24 <arnabshikder2001@gmail.com>
2023-07-06 04:17:08 +0600

    error handling

--- src/app.ts
+++ src/app.ts
@@ -11,11 +11,30 @@ app.use(express.urlencoded({ extended: true }))
 //Application Route
 app.use('/api/v1/users', userRouter)
 
+// class ApiError extends Error {
+//   statusCode: number;
+//   constructor(statusCode: number, message: string | undefined, stack = '') {
+//     super(message);
+//     this.statusCode = statusCode;
+//     if (stack) {
+//       this.stack = stack;
+//     } else {
+//       Error.captureStackTrace(this, this.constructor);
+//     }
+//   }
+// }
+
 app.get('/', async (req: Request, res: Response) => {
   res.send({
     status: true,
     message: 'Server Successfully Run',
   })
+  // throw new ApiError(400, 'error vaiaya error')
 })
+
+// Global error handle
+// app.use((err, req, res, next) => {
+
+// })
 
 export default app
