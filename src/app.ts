3561ab244df0f90d3066a6295e6176c95155ba3c
ArnabShikder24 <arnabshikder2001@gmail.com>
2023-08-09 21:57:12 +0600

    validate request added zod

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { Application } from 'express'
 import cors from 'cors'
 import globalErrorHandler from './app/middlewares/globalErrorHandler'
 import { UserRoutes } from './app/modules/user/user.route'
+import validateRequest from './app/middlewares/validateRequest'
+import { UserValidation } from './app/modules/user/user.validation'
 
 const app: Application = express()
 
@@ -10,7 +12,11 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 //Application Route
-app.use('/api/v1/users', UserRoutes)
+app.use(
+  '/api/v1/users',
+  validateRequest(UserValidation.createUserZodScrema),
+  UserRoutes
+)
 
 // Global error handle
 app.use(globalErrorHandler)
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,18 +1,8 @@
 import { RequestHandler } from 'express'
 import { UserService } from './user.service'
-import { z } from 'zod'
 
 const createUser: RequestHandler = async (req, res, next) => {
   try {
-    const createUserZodScrema = z.object({
-      body: z.object({
-        role: z.string({
-          required_error: 'role is required',
-        }),
-        password: z.string().optional(),
-      }),
-    })
-    await createUserZodScrema.parseAsync(req)
     const { user } = req.body
     const result = await UserService.createUser(user)
     res.status(200).json({
